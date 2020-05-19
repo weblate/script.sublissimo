9f22df425cb2c497ef0de156f05bba03b8c70f3b
Allert van Middendorp <drinkblacklabel@gmail.com>
2020-05-19 16:45:52 +0200

    final 0.0.3

--- main.py
+++ main.py
@@ -6,12 +6,9 @@ import logging
 import xbmcaddon
 import xbmcplugin
 import sys
-import xbmcgui
 
 ADDON = xbmcaddon.Addon()
 
-#jan = sys.argv
-#xbmcgui.Dialog().ok("jan", str(jan))
 script.show_dialog()
 
 
